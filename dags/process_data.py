82eee8563bcbbfef6f05f7ea67cea77a6cc7245d
supratim94336 <supratim.gre2014@yahoo.com>
2024-07-11 20:43:42 +0200

    rename var

--- dags/process_data.py
+++ dags/process_data.py
@@ -4,11 +4,11 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 def process_data_function():
     s3_bucket = 'etl'
-    s3_prefix_llist = ['poke_api/items_attribute', 'poke_api/items']
+    s3_prefix_list = ['poke_api/items_attribute', 'poke_api/items']
 
     s3_client = S3Hook(aws_conn_id='aws_s3')
 
-    for s3_prefix in s3_prefix_llist:
+    for s3_prefix in s3_prefix_list:
         s3_files = s3_client.list_keys(bucket_name=s3_bucket, prefix=s3_prefix)
 
         for s3_file in s3_files:
